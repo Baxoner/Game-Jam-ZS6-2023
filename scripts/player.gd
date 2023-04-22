6d2bff7c82f95494e6ccb76afa2358d544d6805b
Baxoner <baxoner5050@gmail.com>
2023-04-22 23:03:06 +0200

    Early development, working on tilemap right now

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,8 +2,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
 
+func _input(event):
+	if event.is_action_pressed("Shoot") and not $AnimationPlayer.is_playing():
+		$AnimationPlayer.play("gunShot")
+
+
+func _process(delta):
+	$Gun.look_at(get_global_mouse_position())
+
+
+func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var directionX = Input.get_axis("WalkLeft", "WalkRight")
@@ -17,4 +26,12 @@ func _physics_process(delta):
 		velocity.y = directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+		
+	if directionX > 0:
+		$PlayerSprite.flip_h = true
+	else:
+		$PlayerSprite.flip_h = false
+
 	move_and_slide()
+	
+	
